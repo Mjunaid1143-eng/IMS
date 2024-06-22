9d9c40cd8784e14410d979943bbe9b40531cd6e6
junaid iqbal <junaidiqbal1143@hotmail.com>
2024-06-22 14:38:23 +0500

    Add project files.

---
+++ IMS.UseCases/Inventories/ViewInventoriesByNameUseCase.cs
@@ -0,0 +1,29 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.Inventories.Interfaces;
+using IMS.UseCases.PluginInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.UseCases.Inventories
+{
+    public class ViewInventoriesByNameUseCase : IViewInventoriesByNameUseCase
+    {
+        private readonly IInventoryRepository inventoryRepository;
+
+
+
+        //ctor will create constructor
+        public ViewInventoriesByNameUseCase(IInventoryRepository inventoryRepository)
+        {
+            this.inventoryRepository = inventoryRepository;
+        }
+
+        public async Task<IEnumerable<Inventory>> ExecuteAsync(string name = "")
+        {
+            return await inventoryRepository.GetInventoriesByNameAsync(name);
+        }
+    }
+}
